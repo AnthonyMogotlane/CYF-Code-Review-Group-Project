9fa596bb31e69967d5dcc38515c56b39d3844832
Anthony Mogotlane <almogotlane@gmail.com>
2023-01-31 22:07:00 +0200

    Added more tests

--- src/intersection/anthony-intersection-solution/intersection.spec.js
+++ src/intersection/anthony-intersection-solution/intersection.spec.js
@@ -7,4 +7,13 @@ describe('should return values shared between two arrays', () => {
   it('[2, 1, 2] and [2, 3] -> [2]', () => {
     expect([2]).toEqual(intersection([2, 1, 2], [2, 3]));
   });
+  it('[5, 5, 2, 2] and [2, 3, 4, 5] -> [5, 2]', () => {
+    expect([5, 2]).toEqual(intersection([5, 5, 2, 2], [2, 3, 4, 5]));
+  });
+  it('[5, 5, 1, 2, 3] and [3, 4, 5] -> [5, 3]', () => {
+    expect([5, 3]).toEqual(intersection([5, 5, 1, 2, 3], [3, 4, 5]));
+  });
+  it('[10, 11, 1, 2, 3, 5] and [3, 4, 6, 10, 11] -> [10, 11, 3]', () => {
+    expect([10, 11, 3]).toEqual(intersection([10, 11, 1, 2, 3, 5], [3, 4, 6, 10, 11]));
+  });
 });
