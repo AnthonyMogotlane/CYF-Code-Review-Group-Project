522a6363b844b30301536a4271bddbdf095b1a4e
Anthony Mogotlane <almogotlane@gmail.com>
2023-01-31 21:47:34 +0200

    Write tests for intersection function

--- src/intersection/anthony-intersection-solution/intersection.spec.js
+++ src/intersection/anthony-intersection-solution/intersection.spec.js
@@ -1,14 +1,10 @@
-const intersection1 = require('./intersection.js');
+import { intersection } from './intersection.js';
 
 describe('should return values shared between two arrays', () => {
   it('[2, 1] and [2, 3] -> [2]', () => {
-    const expected = [2];
-    const received = intersection1([2, 1], [2, 3]);
-    expect(received).toEqual(expected);
+    expect([2]).toEqual(intersection([2, 1], [2, 3]));
   });
   it('[2, 1, 2] and [2, 3] -> [2]', () => {
-    const expected = [2];
-    const received = intersection1([2, 1, 2], [2, 3]);
-    expect(received).toEqual(expected);
+    expect([2]).toEqual(intersection([2, 1, 2], [2, 3]));
   });
 });
