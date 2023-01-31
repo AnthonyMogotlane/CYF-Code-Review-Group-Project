ec4e573de4f5cf82166f9c606d2263a4bada377c
Anthony Mogotlane <almogotlane@gmail.com>
2023-01-31 21:49:41 +0200

    Added intersection solution

--- src/intersection/anthony-intersection-solution/intersection.js
+++ src/intersection/anthony-intersection-solution/intersection.js
@@ -1,5 +1,10 @@
-const intersection1 = (arr1, arr2) => {
-  return [];
+// Function to remove duplicates on the array
+const removeDuplicate = (arr) => arr.filter((e, i) => arr.indexOf(e) === i);
+// Function to return an array of values shared between two arrays
+export const intersection = (arr1, arr2) => {
+  const result = [];
+  for (const num of removeDuplicate(arr1)) {
+    if (removeDuplicate(arr2).includes(num)) result.push(num);
+  }
+  return result;
 };
-
-exports.module = intersection1;
