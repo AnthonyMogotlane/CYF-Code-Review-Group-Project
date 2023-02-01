9aea06ff21a44be143f05af995319a92ac2435b7
Anthony Mogotlane <almogotlane@gmail.com>
2023-02-01 21:09:55 +0200

    Added default parameters

--- src/intersection/anthony-intersection-solution/intersection.js
+++ src/intersection/anthony-intersection-solution/intersection.js
@@ -1,7 +1,7 @@
 // Function to remove duplicates on the array
 const removeDuplicate = (arr) => arr.filter((e, i) => arr.indexOf(e) === i);
 // Function to return an array of values shared between two arrays
-export const intersection = (arr1, arr2) => {
+export const intersection = (arr1 = [2, 1, 2], arr2 = [2, 3]) => {
   const result = [];
   for (const num of removeDuplicate(arr1)) {
     if (removeDuplicate(arr2).includes(num)) result.push(num);
