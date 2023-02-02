437ee08a333864f20bfb6fa2ffabd29e503036df
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-02-02 10:59:26 +0200

    changes made after review

--- src/difference/tersia-difference/difference.js
+++ src/difference/tersia-difference/difference.js
@@ -1,7 +1,10 @@
 export const difference = () => {
-  let array = [1, 2, 3];
-  const values = 1;
-
-  array = array.filter((item) => item !== values);
-  return array;
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const secondArray = [1, 5];
+  const result = array.filter((val) => {
+    return !secondArray.find((a) => {
+      return val === a;
+    });
+  });
+  return result;
 };
